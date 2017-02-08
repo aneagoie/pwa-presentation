c8e6612a1c65462fa34b0f9e4070c464edf6a9fb
Andrei <aneagoie1@gmail.com>
2017-02-08 11:29:33 -0500

    adding a few more slides

--- example/src/index.js
+++ example/src/index.js
@@ -19,7 +19,9 @@ const images = {
   light1: require("../assets/light1.png"),
   pwa: require("../assets/pwa.png"),
   logo: require("../assets/formidable-logo.svg"),
-  light2: require("../assets/light2.png")
+  light2: require("../assets/light2.png"),
+  twitter: require("../assets/twitter.png"),
+  housing: require("../assets/housing.png")
 };
 
 preloader(images);
@@ -37,14 +39,38 @@ export default class Presentation extends React.Component {
             Progressive Web App in 15min
           </Heading>
           <Link href="https://github.com/aneagoie">
-            <Text bold textColor="tertiary">@aneagoie</Text>
+            <Text bold textColor="tertiary">🐦 @andreineagoie</Text>
           </Link>
         </Slide>
-        <Slide id="wait-what" transition={["slide"]} bgColor="black">
+        <Slide transition={["slide"]} bgColor="black">
           <Image src={images.pwa.replace("/", "")} margin="0px auto 40px" height="293px"/>
           <Heading size={2} caps fit textColor="primary" textFont="primary">
             Progressive What App??
           </Heading>
+        </Slide>
+        <Slide transition={["zoom", "fade"]} bgColor="black">
+          <Image src={images.twitter.replace("/", "")} margin="0px auto 40px" height="293px"/>
+          <Appear fid="1">
+            <Text bold textColor="tertiary">
+              Lyft
+            </Text>
+          </Appear>
+          <Appear fid="1">
+            <Text bold textColor="tertiary">
+              Flipkart
+            </Text>
+          </Appear>
+          <Appear fid="1">
+            <Text bold textColor="tertiary">
+              Alibaba
+            </Text>
+          </Appear>
+        </Slide>
+        <Slide bgColor="black">
+          <Heading size={1} caps fit textColor="primary" textFont="primary">
+            Show this to marketing. You are now a God/Goddess:
+          </Heading>
+          <Image src={images.housing.replace("/", "")} margin="0px auto 40px" height="293px"/>
         </Slide>
         <Slide transition={["zoom", "fade"]} bgColor="primary">
           <Link href="https://github.com/aneagoie/react-training-RoboDex">
@@ -75,13 +101,16 @@ export default class Presentation extends React.Component {
             To explore more:
           </Heading>
           <Link href="https://github.com/jeffposnick/create-react-pwa/compare/c-r-a-0.6.0...c-r-pwa-0.6.0">
-            <Text bold textColor="tertiary">Git Diff to add PWA features</Text>
+            <Text bold textColor="tertiary">Git Diff To Add PWA Features</Text>
           </Link>
           <Link href="https://medium.com/@addyosmani/progressive-web-apps-with-react-js-part-i-introduction-50679aef2b12#.umkemaxm7">
-            <Text bold textColor="tertiary">Addy Osmani on PWA+React. 4 Part Series</Text>
+            <Text bold textColor="tertiary">Addy Osmani On PWA+React. 4 Part Series</Text>
           </Link>
           <Link href="https://www.smashingmagazine.com/2016/08/a-beginners-guide-to-progressive-web-apps/">
-            <Text bold textColor="tertiary">Smashing Magazine on PWA</Text>
+            <Text bold textColor="tertiary">Smashing Magazine On PWA</Text>
+          </Link>
+          <Link href="https://aneagoie.github.io/Progressive-Web-App-React/">
+            <Text bold textColor="tertiary">Our Demo For Today</Text>
           </Link>
         </Slide>
         <Slide transition={["slide"]} bgColor="tertiary">
